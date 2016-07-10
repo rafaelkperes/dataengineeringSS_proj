871b4104c26d71d8a55748d52b84fa637e83750b
dani <ni.cavalheiro@gmail.com>
2016-07-10 07:59:56 -0300

    Update parseActresses.py

--- example/parseActresses.py
+++ example/parseActresses.py
@@ -6,7 +6,7 @@ line = ""
 
 # goes to the begin of the actors list
 while re.search('^THE ACTRESSES LIST',line) == None:
-    line = actors_file.readline()
+    line = <.readline()
 #jump 4 rows
 actors_file.readline()
 actors_file.readline()
@@ -37,19 +37,22 @@ while line != "":
     if re.search(r'^[a-zA-Z0-9\$](.*?)[	]', line): #an actor
             
         name = re.search(r'^(.*?)[	]', line)
-        movie = re.search(r'[	][a-zA-Z0-9 \W]*',line)
+        m = re.search(r'[	][a-zA-Z0-9 \W]*',line)
         year = re.search(r'\([0-9?]*\)',line)
         #print name.group()
         a = name.group().strip()
         #a = a.split(',')
         
+        b = m.group().split('(')
+        movie = b[0]
+        
         actorCSV.write(str(idActor)+'|'+str(a)+'\n')
         
         if year:
-            movieCSV.write(str(idMovie)+'|'+str(movie.group().strip())+'|'+str(year.group())+'\n')
+            movieCSV.write(str(idMovie)+'|'+str(movie)+'|'+str(year.group())+'\n')
             
         else:
-            movieCSV.write(str(idMovie)+'|'+str(movie.group().strip())+'|null\n')
+            movieCSV.write(str(idMovie)+'|'+str(movie)+'|null\n')
             
         
         actor_movieCSV.write(str(idActor)+'|'+str(idMovie)+'\n')
@@ -58,21 +61,21 @@ while line != "":
         
     elif re.search(r'[	][a-zA-Z0-9 \W]*', line): #a movie from a actor
         
-        movie = re.search(r'[	][a-zA-Z0-9 \W]*',line)
+        m = re.search(r'[	][a-zA-Z0-9 \W]*',line)
         year = re.search(r'\([0-9?]*\)',line)
+        
+        b = m.group().split('(')
+        movie = b[0]
+        
         if year:
-            movieCSV.write(str(idMovie)+'|'+str(movie.group().strip())+'|'+str(year.group())+'\n')
+            movieCSV.write(str(idMovie)+'|'+str(movie)+'|'+str(year.group())+'\n')
 
         else:
-            movieCSV.write(str(idMovie)+'|'+str(movie.group().strip())+'|null\n')
+            movieCSV.write(str(idMovie)+'|'+str(movie)+'|null\n')
 
 
         actor_movieCSV.write(str(idActor)+'|'+str(idMovie)+'\n')
         idMovie = idMovie + 1
-            
-            
-    line = actors_file.readline()
-
 
 actorCSV.close()
 movieCSV.close()
